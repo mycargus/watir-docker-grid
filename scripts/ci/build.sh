8acf4bb3ce8d8d5652662c532eae44970c2be71d
Michael Hargiss <mdhargiss@gmail.com>
2019-02-15 20:50:24 -0500

    remove superfluous execute_tests.sh; cleanup

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -22,7 +22,7 @@ function cleanup()
 
 trap cleanup INT TERM EXIT
 
-docker-compose build
+docker-compose build --pull
 docker-compose up -d node-chrome node-firefox hub web
 
 # wait for the selenium grid browser nodes to register with the selenium grid hub
